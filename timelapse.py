bca89780e1b5816defb08cf42e607f237aee3e5b
flyb1z0n <flyb1z0n@gmail.com>
2019-10-01 17:31:02 +0300

    small adjustments

--- timelapse.py
+++ timelapse.py
@@ -2,7 +2,9 @@
 
 import time 
 import os 
+import datetime
 
 while True: 
-    os.system('fswebcam -r 1920x1080 -S 20 --no-banner --save /home/pi/%H%M%S.png') 
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S");
+    os.system('fswebcam -r 1920x1080 -S 20 --no-banner --save /home/pi/timelapse/img/' + now + '.png') 
     time.sleep(10) #
