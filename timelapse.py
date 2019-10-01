6ffb4c55e64df9c05737cd3d53a8679d9a15c849
flyb1z0n <flyb1z0n@gmail.com>
2019-10-01 18:00:37 +0300

    added folder distribution

--- timelapse.py
+++ timelapse.py
@@ -5,6 +5,11 @@ import os
 import datetime
 
 while True: 
+    day = datetime.datetime.now().strftime("%Y-%m-%d");
+    directory = '/home/pi/timelapse/img/' + day +'/';
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S");
-    os.system('fswebcam -r 1920x1080 -S 20 --no-banner --save /home/pi/timelapse/img/' + now + '.png') 
-    time.sleep(10) #
+    os.system('fswebcam -r 1920x1080 -S 20 --no-banner --save ' + directory + now + '.png') 
+    time.sleep(10) 
