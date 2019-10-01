c700352efbeb4345b216e9e603eb0a82dff3a34d
flyb1z0n <flyb1z0n@gmail.com>
2019-10-01 17:38:06 +0300

    added time to file name

--- timelapse.py
+++ timelapse.py
@@ -5,6 +5,6 @@ import os
 import datetime
 
 while True: 
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S");
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S");
     os.system('fswebcam -r 1920x1080 -S 20 --no-banner --save /home/pi/timelapse/img/' + now + '.png') 
     time.sleep(10) #
